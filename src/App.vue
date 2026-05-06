88ce54e8e162e13c2aa9b4811f5c66cbc6179d3a
Julian Vleesenbeek <263374522+julianvlsnbk@users.noreply.github.com>
2026-05-06 10:00:44 +0200

    fix: correct formatting of import statements in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
-import { SpeedInsights } from "@vercel/speed-insights/vue" import { Analytics }
-from"@vercel/analytics/vue"
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { SpeedInsights } from '@vercel/speed-insights/vue'
+import { Analytics } from '@vercel/analytics/vue'
+</script>
 
 <template>
+  <Analytics />
+  <SpeedInsights />
   <RouterView />
 </template>
